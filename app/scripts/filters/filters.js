3e15f57d69b053d700cb4130ebedb7ce4c6d9235
xpzhang <xiaopeng.zhang@midea.com>
2017-03-10 19:04:41 +0800

    详情页修改

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -15,3 +15,14 @@ filters.filter('baseUrl', function(){
     }
   };
 });
+filters.filter('getValue', function(){
+  return function(array,key) {
+    for(var i =0;i<array.length;i++){
+      if(key==array[i].key){
+        return array[i].value;
+      }
+    }
+    return '';
+  };
+});
+
